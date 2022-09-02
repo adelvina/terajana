5c89dc84e77fda1e66f1415fa2c08dc98b6927e6
adelvina <103128927+adelvina@users.noreply.github.com>
2022-09-02 14:10:07 +0700

    Update poptera.js

--- poptera.js
+++ poptera.js
@@ -1,1 +1,1 @@
-src='//pl16749796.profitablegatetocontent.com/6b/e2/17/6be2175a5b568f468ff50648084eaacc.js'
+type='text/javascript' src='//pl16749796.profitablegatetocontent.com/6b/e2/17/6be2175a5b568f468ff50648084eaacc.js'
