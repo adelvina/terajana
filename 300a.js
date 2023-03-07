86070a40180f06e1cb19af3c4604ac64e799f961
adelvina <103128927+adelvina@users.noreply.github.com>
2023-03-07 13:35:28 +0700

    Create 300a.js

---
+++ 300a.js
@@ -0,0 +1,8 @@
+atOptions = {
+	'key' : '4893137c396711342c302cf71c3ec167',
+	'format' : 'iframe',
+	'height' : 250,
+	'width' : 300,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/4893137c396711342c302cf71c3ec167/invoke.js"></scr' + 'ipt>');
