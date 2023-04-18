c77a1fd18d95f89f13e44e921f99324d82c5f00a
adelvina <103128927+adelvina@users.noreply.github.com>
2023-04-18 08:10:45 +0700

    Create klika.js

---
+++ klika.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+  $('body').addClass('xepo_ads');
+});
+
+$(document).on('click','.xepo_ads',function(e)
+{
+    $(this).removeClass('xepo_ads');
+    window.open('https://www.highcpmrevenuenetwork.com/iz1aak4x?key=f0828124cee5e58052bf018f3b868724', '_blank');
+});
