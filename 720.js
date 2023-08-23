9da71473651231663999fe283f34d6f8ff9821dd
adelvina <103128927+adelvina@users.noreply.github.com>
2023-08-23 22:22:23 +0700

    Create 720.js

---
+++ 720.js
@@ -0,0 +1,8 @@
+atOptions = {
+	'key' : '586fcd5aef708d31b2c239f013d34fcf',
+	'format' : 'iframe',
+	'height' : 90,
+	'width' : 728,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/586fcd5aef708d31b2c239f013d34fcf/invoke.js"></scr' + 'ipt>');
